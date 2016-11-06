a850202fa6d32644cbace27ca4948abe44964835
dozen <iwatakiosk@gmail.com>
2016-11-06 22:15:58 +0900

    加速度をロギングする

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>加速度センサーの値を取る</title>
+    <style>
+        @import url(http://fonts.googleapis.com/earlyaccess/notosansjapanese.css);
+        html, body{
+            font-size: 18px;
+            font-family: 'Noto Sans Japanese', sans-serif;
+            text-align: center;
+            background-color: #fafafa;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1>重力加速度を除いた加速度 加速度 回転速度</h1>
+</header>
+<main>
+    <div>
+        <div id="interval">
+            <p><span id="i-msec"></span></p>
+        </div>
+        <div id="acceleration">
+            <h2>acceleration[m/s^2]</h2>
+            <p>x: <span id="acc-x"></span></p>
+            <p>y: <span id="acc-y"></span></p>
+            <p>z: <span id="acc-z"></span></p>
+        </div>
+        <div id="accelerationIncludingGravity">
+            <h2>accelerationIncludingGravity[m/s^2]</h2>
+            <p>x: <span id="acc-gx"></span></p>
+            <p>y: <span id="acc-gy"></span></p>
+            <p>z: <span id="acc-gz"></span></p>
+        </div>
+        <div id="rotationRate">
+            <h2>rotationRate[degree/s]</h2>
+            <p>a: <span id="rr-a"></span></p>
+            <p>b: <span id="rr-b"></span></p>
+            <p>g: <span id="rr-g"></span></p>
+        </div>
+    </div>
+</main>
+
+<script src="main.js"></script>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,60 @@
+<?php
+
+class Logger {
+    public $log;
+
+    function __construct() {
+        $this->log = fopen("log.txt", "a");
+    }
+
+    function puts($mes) {
+        fputs($this->log, $mes . "\n");
+    }
+
+    function __destruct() {
+        fclose($this->log);
+    }
+
+}
+
+$l = new Logger();
+
+try {
+    $pdo = new PDO("sqlite:sqlite.db");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("CREATE TABLE IF NOT EXISTS acc(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        accx NUMERIC,
+        accy NUMERIC,
+        accz NUMERIC,
+        gaccx NUMERIC,
+        gaccy NUMERIC,
+        gaccz NUMERIC,
+        rra NUMERIC,
+        rrb NUMERIC,
+        rrg NUMERIC,
+        time INTEGER
+    )");
+
+    $stmt = $pdo->prepare("INSERT INTO acc(accx, accy, accz, gaccx, gaccy, gaccz, rra, rrb, rrg, time) VALUES(:accx, :accy, :accz, :gaccx, :gaccy, :gaccz, :rra, :rrb, :rrg, :time)");
+
+    $data = json_decode($_POST["json"]);
+
+    foreach ($data as $line) {
+        $stmt->bindValue(":accx", $line->a->x);
+        $stmt->bindValue(":accy", $line->a->y);
+        $stmt->bindValue(":accz", $line->a->z);
+        $stmt->bindValue(":gaccx", $line->g->x);
+        $stmt->bindValue(":gaccy", $line->g->y);
+        $stmt->bindValue(":gaccz", $line->g->z);
+        $stmt->bindValue(":rra", $line->r->a);
+        $stmt->bindValue(":rrb", $line->r->b);
+        $stmt->bindValue(":rrg", $line->r->g);
+        $stmt->bindValue(":time", $line->t);
+
+        $stmt->execute();
+    }
+
+} catch (Exception $e) {
+    $l->puts($e->getMessage());
+}
